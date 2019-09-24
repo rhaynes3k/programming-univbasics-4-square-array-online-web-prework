d319fb00454249b068be397238542a9064ced70e
Rommel Haynes <rhaynes3k@gmail.com>
2019-09-24 02:32:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   count = 0
-  while count < array.length(array) do
-    array[count].sqrt
-    puts array
-    count =+ 1
+  while count < array.length do
+    tot = array[count] * array[count]
+    puts tot
+    count += 1
 end
